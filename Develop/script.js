4bd64860fdc29051bf0c149e59e07331218fedc7
Brandy Moore <brandymoore923@gmail.com>
2022-08-17 16:01:58 -0500

    created files and folders. Copied starter code from class work

---
+++ Develop/script.js
@@ -0,0 +1,18 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  //TODO: CODE GOES HERE
+  var numCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # password-generator-project
 03-JavaScript
+
+## Table of Contents
+1. [Description](#Description)
+2. [Visuals](#visuals)
+
+## Description
+
+[Link to live site]()
+
+## Visuals
+![Alt text]()
